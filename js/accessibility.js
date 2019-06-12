d6e7ca103bf101eec11e890faf3091ed7a11961b
Irina Kovalchuk <synthbeing@icloud.com>
2019-06-13 00:02:46 +0300

    Pre-defend fixes part 1

--- js/accessibility.js
+++ js/accessibility.js
@@ -1,4 +1,4 @@
-window.addEventListener('DOMContentLoaded', function () {
+window.addEventListener("DOMContentLoaded", function () {
     //search focus
     let inputSearch = document.querySelector(".search");
     let btnSearch = document.querySelector(".search-btn");
